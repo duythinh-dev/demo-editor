c79fae523117d612da0f3b4171d112e793bccaf5
Duy-Thinh <thinhogg11@gmail.com>
2024-12-11 17:57:38 +0700

    update

--- src/app/slate-editor/page.js
+++ src/app/slate-editor/page.js
@@ -1,7 +1,13 @@
 "use client";
 
-import { Button, Icon, Toolbar } from "../component/slate";
-import React, { useCallback, useMemo, useState } from "react";
+import { Button, Icon, Portal, Toolbar } from "../component/slate";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import isHotkey, { isKeyHotkey } from "is-hotkey";
 import {
   Editable,
@@ -32,7 +38,7 @@ import BackupIcon from "@mui/icons-material/Backup";
 import InsertLinkIcon from "@mui/icons-material/InsertLink";
 import AddLinkIcon from "@mui/icons-material/AddLink";
 import LinkOffIcon from "@mui/icons-material/LinkOff";
-
+import CloseIcon from "@mui/icons-material/Close";
 import {
   Editor,
   Transforms,
@@ -45,6 +51,10 @@ import Image from "next/image";
 import { css } from "@emotion/css";
 import { Dialog, DialogContent, DialogTitle, TextField } from "@mui/material";
 import { renderJSONToHTML } from "./helper";
+import { CHARACTERS } from "../component/slate/constant";
+import { IS_MAC } from "../component/utils/environment";
+
+const allowedSchemes = ["http:", "https:"];
 
 const HOTKEYS = {
   "mod+b": "bold",
@@ -111,9 +121,7 @@ const isMarkActive = (editor, format) => {
   const marks = Editor.marks(editor);
   return marks ? marks[format] === true : false;
 };
-const ImageRender = ({ attributes, children, element }) => {
-  const editor = useSlateStatic();
-  console.log(editor, element);
+const ImageRender = ({ attributes, children, element, editor }) => {
   const path = ReactEditor.findPath(editor, element);
   const selected = useSelected();
   const focused = useFocused();
@@ -126,8 +134,10 @@ const ImageRender = ({ attributes, children, element }) => {
           position: relative;
         `}
       >
-        <img
+        <Image
           src={element.url}
+          width={100}
+          height={100}
           className={css`
             display: block;
             max-width: 100%;
@@ -140,14 +150,14 @@ const ImageRender = ({ attributes, children, element }) => {
           active
           onClick={() => Transforms.removeNodes(editor, { at: path })}
           className={css`
-            display: ${selected && focused ? "inline" : "none"};
+            display: "inline";
             position: absolute;
             top: 0.5em;
             left: 0.5em;
             background-color: white;
           `}
         >
-          <Icon>delete</Icon>
+          <CloseIcon />
         </Button>
       </div>
     </div>
@@ -189,11 +199,88 @@ const ButtonLink = ({ format, icon }) => {
   );
 };
 
-const Element = ({ attributes, children, element }) => {
+const VideoElement = ({ attributes, children, element }) => {
+  const editor = useSlateStatic();
+  const { url } = element;
+  const safeUrl = useMemo(() => {
+    let parsedUrl = null;
+    try {
+      parsedUrl = new URL(url);
+      // eslint-disable-next-line no-empty
+    } catch {}
+    if (parsedUrl && allowedSchemes.includes(parsedUrl.protocol)) {
+      return parsedUrl.href;
+    }
+    return "about:blank";
+  }, [url]);
+
+  const UrlInput = ({ url, onChange }) => {
+    const [value, setValue] = React.useState(url);
+    return (
+      <input
+        value={value}
+        onClick={(e) => e.stopPropagation()}
+        style={{
+          marginTop: "5px",
+          boxSizing: "border-box",
+        }}
+        onChange={(e) => {
+          const newUrl = e.target.value;
+          setValue(newUrl);
+          onChange(newUrl);
+        }}
+      />
+    );
+  };
+
+  return (
+    <div {...attributes}>
+      <div contentEditable={false}>
+        <div
+          style={{
+            padding: "75% 0 0 0",
+            position: "relative",
+          }}
+        >
+          <iframe
+            src={`${safeUrl}?title=0&byline=0&portrait=0`}
+            frameBorder="0"
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </div>
+        <UrlInput
+          url={url}
+          onChange={(val) => {
+            const path = ReactEditor.findPath(editor, element);
+            const newProperties = {
+              url: val,
+            };
+            Transforms.setNodes(editor, newProperties, {
+              at: path,
+            });
+          }}
+        />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+const Element = ({ attributes, children, element, editor }) => {
   const style = { textAlign: element.align };
   switch (element.type) {
     case "image":
-      return <ImageRender {...attributes} element={element} />;
+      return <ImageRender {...attributes} element={element} editor={editor} />;
+    case "video":
+      return <VideoElement {...attributes} element={element} editor={editor} />;
+    case "mention":
+      return <Mention {...attributes} element={element} editor={editor} />;
     case "block-quote":
       return (
         <blockquote style={style} {...attributes}>
@@ -335,6 +422,77 @@ const MarkButton = ({ format, icon }) => {
     </Button>
   );
 };
+
+const withMentions = (editor) => {
+  const { isInline, isVoid, markableVoid } = editor;
+  editor.isInline = (element) => {
+    return element.type === "mention" ? true : isInline(element);
+  };
+  editor.isVoid = (element) => {
+    return element.type === "mention" ? true : isVoid(element);
+  };
+  editor.markableVoid = (element) => {
+    return element.type === "mention" || markableVoid(element);
+  };
+  return editor;
+};
+const insertMention = (editor, character) => {
+  const mention = {
+    type: "mention",
+    character,
+    children: [{ text: "" }],
+  };
+  Transforms.insertNodes(editor, mention);
+  Transforms.move(editor);
+};
+
+const Mention = ({ attributes, children, element }) => {
+  const selected = useSelected();
+  const focused = useFocused();
+  const style = {
+    padding: "3px 3px 2px",
+    margin: "0 1px",
+    verticalAlign: "baseline",
+    display: "inline-block",
+    borderRadius: "4px",
+    backgroundColor: "#eee",
+    fontSize: "0.9em",
+    boxShadow: selected && focused ? "0 0 0 2px #B4D5FF" : "none",
+  };
+  // See if our empty text child has any styling marks applied and apply those
+  if (element.children[0].bold) {
+    style.fontWeight = "bold";
+  }
+  if (element.children[0].italic) {
+    style.fontStyle = "italic";
+  }
+  return (
+    <span
+      {...attributes}
+      contentEditable={false}
+      data-cy={`mention-${element.character.replace(" ", "-")}`}
+      style={style}
+    >
+      {/* Prevent Chromium from interrupting IME when moving the cursor */}
+      {/* 1. span + inline-block 2. div + contenteditable=false */}
+      <div contentEditable={false}>
+        {IS_MAC ? (
+          // Mac OS IME https://github.com/ianstormtaylor/slate/issues/3490
+          <>
+            {children}@{element.character}
+          </>
+        ) : (
+          // Others like Android https://github.com/ianstormtaylor/slate/pull/5360
+          <>
+            @{element.character}
+            {children}
+          </>
+        )}
+      </div>
+    </span>
+  );
+};
+
 const initialValue = [
   {
     type: "paragraph",
@@ -367,6 +525,11 @@ const initialValue = [
     type: "paragraph",
     align: "center",
     children: [{ text: "Try it out for yourself!" }],
+  },
+  {
+    type: "video",
+    url: "https://player.vimeo.com/video/26689853",
+    children: [{ text: "" }],
   },
 ];
 
@@ -433,7 +596,14 @@ const wrapLink = (editor, url) => {
   }
 };
 
-const withInlines = (editor) => {
+const withEmbeds = (editor) => {
+  const { isVoid } = editor;
+  editor.isVoid = (element) =>
+    element.type === "video" ? true : isVoid(element);
+  return editor;
+};
+
+const withInLines = (editor) => {
   const { insertData, insertText, isInline, isElementReadOnly, isSelectable } =
     editor;
   editor.isInline = (element) =>
@@ -519,17 +689,99 @@ const ToggleEditableButtonButton = () => {
 };
 
 const RichTextExample = ({ onChange }) => {
-  const renderElement = useCallback((props) => <Element {...props} />, []);
-  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+  const ref = useRef();
+  const [search, setSearch] = useState("");
+  const [target, setTarget] = useState();
+  const [index, setIndex] = useState(0);
+
   const editor = useMemo(
-    () => withInlines(withHistory(withReact(withImages(createEditor())))),
+    () =>
+      withMentions(
+        withEmbeds(
+          withInLines(withHistory(withReact(withImages(createEditor()))))
+        )
+      ),
     []
   );
+  const renderElement = useCallback(
+    (props) => <Element {...props} editor={editor} />,
+    []
+  );
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
 
+  const chars = CHARACTERS.filter((c) =>
+    c.toLowerCase().startsWith(search.toLowerCase())
+  ).slice(0, 10);
+  const onKeyDown = useCallback(
+    (event) => {
+      if (target && chars.length > 0) {
+        switch (event.key) {
+          case "ArrowDown":
+            event.preventDefault();
+            const prevIndex = index >= chars.length - 1 ? 0 : index + 1;
+            setIndex(prevIndex);
+            break;
+          case "ArrowUp":
+            event.preventDefault();
+            const nextIndex = index <= 0 ? chars.length - 1 : index - 1;
+            setIndex(nextIndex);
+            break;
+          case "Tab":
+          case "Enter":
+            event.preventDefault();
+            Transforms.select(editor, target);
+            insertMention(editor, chars[index]);
+            setTarget(null);
+            break;
+          case "Escape":
+            event.preventDefault();
+            setTarget(null);
+            break;
+        }
+      }
+    },
+    [chars, editor, index, target]
+  );
+
+  useEffect(() => {
+    if (target && chars.length > 0) {
+      const el = ref.current;
+      const domRange = ReactEditor.toDOMRange(editor, target);
+      const rect = domRange.getBoundingClientRect();
+      el.style.top = `${rect.top + window.pageYOffset + 24}px`;
+      el.style.left = `${rect.left + window.pageXOffset}px`;
+    }
+  }, [chars.length, editor, index, search, target]);
   // how to get the value of the editor - when onChange is triggered
 
   return (
-    <Slate editor={editor} initialValue={initialValue} onChange={onChange}>
+    <Slate
+      editor={editor}
+      initialValue={initialValue}
+      onChange={(e) => {
+        onChange(e);
+        const { selection } = editor;
+        if (selection && Range.isCollapsed(selection)) {
+          const [start] = Range.edges(selection);
+          const wordBefore = Editor.before(editor, start, { unit: "word" });
+          const before = wordBefore && Editor.before(editor, wordBefore);
+          const beforeRange = before && Editor.range(editor, before, start);
+          const beforeText = beforeRange && Editor.string(editor, beforeRange);
+          const beforeMatch = beforeText && beforeText.match(/^@(\w+)$/);
+          const after = Editor.after(editor, start);
+          const afterRange = Editor.range(editor, start, after);
+          const afterText = Editor.string(editor, afterRange);
+          const afterMatch = afterText.match(/^(\s|$)/);
+          if (beforeMatch && afterMatch) {
+            setTarget(beforeRange);
+            setSearch(beforeMatch[1]);
+            setIndex(0);
+            return;
+          }
+        }
+        setTarget(null);
+      }}
+    >
       <Toolbar>
         <MarkButton format="bold" icon={<FormatBoldIcon />} />
         <MarkButton format="italic" icon={<FormatItalicIcon />} />
@@ -557,6 +809,7 @@ const RichTextExample = ({ onChange }) => {
         spellCheck
         autoFocus
         onKeyDown={(event) => {
+          onKeyDown(event);
           const { selection } = editor;
           for (const hotkey in HOTKEYS) {
             if (isHotkey(hotkey, event)) {
@@ -580,6 +833,43 @@ const RichTextExample = ({ onChange }) => {
           }
         }}
       />
+      {target && chars.length > 0 && (
+        <Portal>
+          <div
+            ref={ref}
+            style={{
+              top: "-9999px",
+              left: "-9999px",
+              position: "absolute",
+              zIndex: 1,
+              padding: "3px",
+              background: "white",
+              borderRadius: "4px",
+              boxShadow: "0 1px 5px rgba(0,0,0,.2)",
+            }}
+            data-cy="mentions-portal"
+          >
+            {chars.map((char, i) => (
+              <div
+                key={char}
+                onClick={() => {
+                  Transforms.select(editor, target);
+                  insertMention(editor, char);
+                  setTarget(null);
+                }}
+                style={{
+                  padding: "1px 3px",
+                  borderRadius: "3px",
+                  cursor: "pointer",
+                  background: i === index ? "#B4D5FF" : "transparent",
+                }}
+              >
+                {char}
+              </div>
+            ))}
+          </div>
+        </Portal>
+      )}
     </Slate>
   );
 };
